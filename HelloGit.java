a23321968f0e96bf06e826506658a68b7618c022
paranmin <enhoomin@naver.com>
2018-01-31 10:48:56 +0900

    github add 1 line

--- HelloGit.java
+++ HelloGit.java
@@ -2,6 +2,7 @@ package git_test;
 
 // Local modification
 // Github modification
+// github add 1 line
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
